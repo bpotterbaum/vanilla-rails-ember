8bdb7f53daf9d9c736c7840954d02bd33dcacf5c
Brad Potterbaum <bpotterbaum@gmail.com>
2013-04-08 09:51:23 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VanillaRailsEmber::Application.config.secret_token = '47dfdcb4d2ef492fb88fbebc2376878b2f3fc7e3dc4ace22f7ddc6306ed366f990eb58442f9f0cb6754637b0e040735877ae42958f6c7a29424c0c5c1ce50271'
